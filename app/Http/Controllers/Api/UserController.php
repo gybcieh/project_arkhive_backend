61cefe5b9cdc3d9613da93902b89669ac73c4cd3
gybcieh <jvlauronilla@gmail.com>
2025-09-15 14:47:24 +0800

    Added user update

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -18,4 +18,33 @@ class UserController extends Controller
     public function show(User $user){
         return new UserResource($user);
     }
+
+    public function update(Request $request, User $user){
+        $fields = $request->validate([
+            'mobile_number' => 'sometimes|string|unique:users,mobile_number,'.$user->id,
+            'email_address' => 'sometimes|string|unique:users,email_address,'.$user->id,
+            'username' => 'sometimes|string|unique:users,username,'.$user->id,
+            'password' => 'sometimes|string|confirmed'
+        ]);
+
+        if(isset($fields['mobile_number'])){
+            $user->mobile_number = $fields['mobile_number'];
+        }
+        if(isset($fields['email_address'])){
+            $user->email_address = $fields['email_address'];
+        }
+        if(isset($fields['username'])){
+            $user->username = $fields['username'];
+        }
+        if(isset($fields['password'])){
+            $user->password_digest = $fields['password'];
+        }
+
+        $user->save();
+
+        return [
+            'message' => 'User updated successfully',
+            'data' => new UserResource($user)
+        ];
+    }
 }
